f99a2b7f582c216117be1cd7ef44326b98bc2aba
sbalun <sbalun@gmail.com>
2025-01-13 18:01:34 -0500

    additional-linear-algebra-operations and numpy review

---
+++ additional-linear-algebra-operations.py
@@ -0,0 +1,55 @@
+import numpy as np
+#  We will be using the numpy.linalg sublibrary to perform the following operations:
+#
+# The “norm” (or length/magnitude) of a vector can be found using np.linalg.norm():
+
+v = np.array([2,-4,1])
+print("\nGiven array([2,-4,1]):")
+v_norm = np.linalg.norm(v)
+print("\nv_norm or length/magnitude of vector: \n", v_norm)
+
+# The inverse of a square matrix, if one exists, can be found using np.linalg.inv():
+
+A = np.array([[1,2],[3,4]])
+print("\nThe inverse square matrix of A: \n", np.linalg.inv(A))
+
+'''
+Finally, we can actually solve for unknown variables in a system on linear equations in Ax=b form
+using np.linalg.solve(), which takes in the A and b parameters. Given:
+
+x + 4y -  z = -1
+-x - 3y + 2z = 2
+2x -  y - 2z = -2
+   
+We convert to Ax=b form and solve.
+'''
+# each array in A is an equation from the above system of equations
+A = np.array([[1,4,-1],[-1,-3,2],[2,-1,-2]])
+# the solution to each equation
+b = np.array([-1,2,-2])
+# solve for x, y, and z
+x,y,z = np.linalg.solve(A,b)
+
+print("\nThe solution is: \n",(x, y, z))
+
+# Given
+'''
+4x + z = 2
+-y + 2z - 3x = 0
+.5y - x - 1.5z = -4
+
+Need to list in same order x, y, z.  So rearranged it is
+ 4x + 0y + 1z  = 2
+-3x - 1y + 2z  = 0
+-1x +.5y - 1.5z = -4
+'''
+A = np.array([[4,0,1],[-3,-1,2],[-1,.5,-1.5]])
+
+'''
+We now need the b part of our Ax=b form.
+'''
+b = np.array([2,0,-4])
+
+x,y,z = np.linalg.solve(A,b)
+
+print("\nThe next solution is: \n",(x, y, z))
---
+++ numpy-review.py
@@ -0,0 +1,28 @@
+import numpy as np
+"""
+The Python library NumPy provides a powerful tool to programmatically perform linear algebra.
+A fundamental building block of NumPy, the NumPy array allows us to represent vectors and matrices.
+
+These arrays can perform operations directly, such as addition
+and scalar multiplication using + and *, respectively.
+
+NumPy also has built-in functions that allow us to perform more advanced operations
+like dot products, transposes, and matrix multiplication.
+
+Finally, NumPy has a numpy.linalg sublibrary that provides more advanced functionality.
+This includes finding the norm (or magnitude/length) of a vector, 
+finding the inverse of a square matrix, and solving a system of linear equations in Ax=b form.
+
+Given the following system of equations:
+
+ 2a + 0b - 2c + 3d = 4
+-1a + 4b - 1c + 0d = 1
+ 3a - 1b - 2c + 2d = 2
+-2a - 1b + 3c - 0d = -2
+
+Find the unknowns a, b, c, and d. 
+"""
+A = np.array([[2,0,-2,3],[-1,4,-1,0],[3,-1,-2,2],[-2,-1,3,0]])
+b = np.array([4,1,2,-2])
+a,b,c,d = np.linalg.solve(A,b)
+print((a,b,c,d))
